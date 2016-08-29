850495548841acc821386cf6736399de4cb26284
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2016-08-28 22:40:08 -0400

    markdown all div.text tags

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+  has_many :comments
   has_and_belongs_to_many :liked_posts, join_table: :user_post_likes, class_name: "Post"
   has_and_belongs_to_many :liked_comments, join_table: :user_comment_likes, class_name: "Comment"
   
